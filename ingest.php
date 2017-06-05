c3df2c5ece44a8a44ad4a4a50e4dac8cf5a74142
root <root@candidate.playground-tyler.alway>
2017-06-05 23:32:22 +0000

    Added comments to the ingest.php file

--- ingest.php
+++ ingest.php
@@ -4,14 +4,12 @@ require 'Predis/Autoloader.php';
 
 Predis\Autoloader::register();
 
-
-$req = $_SERVER['REQUEST_METHOD'];
-switch ($req){
-  case 'GET':
-    print("TODO GET\n");
-    break;
-  case 'POST':
-
+//Switch statement to determine the type of request recived
+//the only type of request accepted by this program will be POST requests 
+switch ($_SERVER['REQUEST_METHOD']){
+  case 'POST':i
+  //open the connection the redis server (postback queue)
+   //TODO need to add authentication 
   try{
     $redis = new Predis\Client([
         'scheme' => 'tcp',
@@ -19,23 +17,27 @@ switch ($req){
         'port'   => 6369,
     ]);
     
+    //Get the post data from the request and convert it from a string->json (array in php)
     $obj =  json_decode(file_get_contents('php://input'),true);
+    //store the endpoint object to be sent to the queue
     $front = $obj["endpoint"];
-    $front = json_encode($front); //turns the return object into a string
+    $front = json_encode($front); 
+
+    //loop through the data section appending the current data object to front and send them to the queue
     foreach ($obj["data"] as $x) {  
       $temp = json_encode($x);
       $temp = substr($front, 0, strlen($front)-1) . ",\"data\":" .$temp . "}";
       $redis->lpush('data', $temp);
     }    
-
+  //if the redis connection fails
   } catch (Exception $e) {
     echo "Couldn't connected to Redis\n";
     echo $e->getMessage();
   }
-
-
     break;
+  
+  //a default case to catch all other types of http requests
   default: 
-    print("TODO everything else\n");
+    print("Error not a POST request\n");
 }
 ?>
