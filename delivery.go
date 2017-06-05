529443fc1e60867c921b780379379b45a3d0012a
root <root@candidate.playground-tyler.alway>
2017-06-05 18:07:56 +0000

    Basic connection to redis server implemented

--- delivery.go
+++ delivery.go
@@ -1,13 +1,32 @@
 package main
 
-import "fmt"
-
-
-
+import (
+  "fmt"
+  "github.com/garyburd/redigo/redis"
+  "time"
+)
 
 func main() {
 
-  fmt.Println("here");
+  fmt.Println("Delivery server started \nPress Ctrl-C to quit.")
+
+
+  client, err := redis.Dial("tcp", ":6369")
+  if err != nil {
+    panic(err)
+  }
+  defer client.Close()
+
+  for {
+    pushback, err := client.Do("RPOP", "test")
+
+    if pushback != nil {
+      fmt.Println(redis.String(pushback, err))
+    } else {
+      fmt.Println("the queue is empty")
+    }
+    time.Sleep(1000 * time.Millisecond)
+  }
 }
 
 
