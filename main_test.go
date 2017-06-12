2bd60312fb1952124f40a8551d8779a9dad00b49
Tyler Alway <tyleralway@Kochavas-MacBook-Pro.local>
2017-06-12 08:55:00 -0700

    improved unit tests

--- main_test.go
+++ main_test.go
@@ -10,28 +10,76 @@ type testpair struct {
   result string
 }
 
-var urlTests = []testpair{
-        {data: postback{Method:"GET", Url: "https://httpbin.org/get?evil={$money}", Data: map[string]string{"$money": "100 dollars"}}, result:"https://httpbin.org/get?evil=100+dollars"},
-        {data: postback{Method:"GET", Url: "https://httpbin.org/get?evil={$money}"}, result:"https://httpbin.org/get?evil="},
-        {data: postback{Method:"GET", Url: "https://httpbin.org/get?evil="}, result:"https://httpbin.org/get?evil="},
-        {data: postback{Method:"GET", Url: "", Data: map[string]string{"$money": "100 dollars"}}, result:""},
-        {data: postback{Method:"GET", Url: "https://httpbin.org/get?evil="}, result:"https://httpbin.org/get?evil="},
-        {data: postback{Method:"GET", Url: "https://httpbin.org/get?evil={$money}&other={other}", Data: map[string]string{"$money": "100 dollars", "other":"something else"}}, result:"https://httpbin.org/get?evil=100+dollars&other=something+else"},
-        {data: postback{Method:"GET", Url: "https://httpbin.org/get?evil={$money}&other={other}"}, result:"https://httpbin.org/get?evil=&other="},
-        {data: postback{Method:"GET", Url: "https://httpbin.org/get?evil={$money}&other={other}", Data: map[string]string{"other":"something else"}}, result:"https://httpbin.org/get?evil=&other=something+else"},
-        {data: postback{Method:"GET", Url: "{.*}", Data: map[string]string{".*": "100 dollars", "other":"something else"}}, result:"100+dollars"},
-        {data: postback{Method:"GET", Url: "{$money*}{other?}", Data: map[string]string{"$money*": "100 dollars", "other?":"something else"}}, result:"100+dollarssomething+else"},
-
-}
-
-
 
 func TestFormatUrl(t *testing.T) {
+  pair := testpair{data: postback{Method:"GET", Url: "https://httpbin.org/get?evil={$money}", Data: map[string]string{"$money": "100 dollars"}}, result:"https://httpbin.org/get?evil=100+dollars"}
 
-        for _, pair := range urlTests {
-                r := formatUrl(pair.data)
-                if (strings.Compare(r.Url, pair.result) != 0) {
-                        t.Errorf("The url wasn't formatted correctly. was expecting %s got %s", pair.result, r.Url)
-                }
-        }
+  r := formatUrl(pair.data)
+  if (strings.Compare(r.Url, pair.result) != 0) {
+    t.Errorf("The url wasn't formatted correctly. was expecting %s got %s", pair.result, r.Url)
+  }
+
+  pair = testpair{data: postback{Method:"GET", Url: "https://httpbin.org/get?evil={$money}"}, result:"https://httpbin.org/get?evil="}
+
+  r = formatUrl(pair.data)
+  if (strings.Compare(r.Url, pair.result) != 0) {
+    t.Errorf("The url wasn't formatted correctly. was expecting %s got %s", pair.result, r.Url)
+  }
+
+  pair = testpair{data: postback{Method:"GET", Url: "https://httpbin.org/get?evil="}, result:"https://httpbin.org/get?evil="}
+
+  r = formatUrl(pair.data)
+  if (strings.Compare(r.Url, pair.result) != 0) {
+    t.Errorf("The url wasn't formatted correctly. was expecting %s got %s", pair.result, r.Url)
+  }
+
+  pair = testpair{data: postback{Method:"GET", Url: "", Data: map[string]string{"$money": "100 dollars"}}, result:""}
+
+  r = formatUrl(pair.data)
+  if (strings.Compare(r.Url, pair.result) != 0) {
+    t.Errorf("The url wasn't formatted correctly. was expecting %s got %s", pair.result, r.Url)
+  }
+  pair = testpair{data: postback{Method:"GET", Url: "https://httpbin.org/get?evil="}, result:"https://httpbin.org/get?evil="}
+
+
+  r = formatUrl(pair.data)
+  if (strings.Compare(r.Url, pair.result) != 0) {
+    t.Errorf("The url wasn't formatted correctly. was expecting %s got %s", pair.result, r.Url)
+  }
+  pair = testpair{data: postback{Method:"GET", Url: "https://httpbin.org/get?evil={$money}&other={other}", Data: map[string]string{"$money": "100 dollars", "other":"something else"}}, result:"https://httpbin.org/get?evil=100+dollars&other=something+else"}
+
+
+  r = formatUrl(pair.data)
+  if (strings.Compare(r.Url, pair.result) != 0) {
+    t.Errorf("The url wasn't formatted correctly. was expecting %s got %s", pair.result, r.Url)
+  }
+  pair = testpair{data: postback{Method:"GET", Url: "https://httpbin.org/get?evil={$money}&other={other}"}, result:"https://httpbin.org/get?evil=&other="}
+
+
+  r = formatUrl(pair.data)
+  if (strings.Compare(r.Url, pair.result) != 0) {
+    t.Errorf("The url wasn't formatted correctly. was expecting %s got %s", pair.result, r.Url)
+  }
+  pair = testpair{data: postback{Method:"GET", Url: "https://httpbin.org/get?evil={$money}&other={other}", Data: map[string]string{"other":"something else"}}, result:"https://httpbin.org/get?evil=&other=something+else"}
+
+
+  r = formatUrl(pair.data)
+  if (strings.Compare(r.Url, pair.result) != 0) {
+    t.Errorf("The url wasn't formatted correctly. was expecting %s got %s", pair.result, r.Url)
+  }
+  pair = testpair{data: postback{Method:"GET", Url: "{.*}", Data: map[string]string{".*": "100 dollars", "other":"something else"}}, result:"100+dollars"}
+
+
+  r = formatUrl(pair.data)
+  if (strings.Compare(r.Url, pair.result) != 0) {
+    t.Errorf("The url wasn't formatted correctly. was expecting %s got %s", pair.result, r.Url)
+  }
+  pair = testpair{data: postback{Method:"GET", Url: "{$money*}{other?}", Data: map[string]string{"$money*": "100 dollars", "other?":"something else"}}, result:"100+dollarssomething+else"}
+
+
+  r = formatUrl(pair.data)
+  if (strings.Compare(r.Url, pair.result) != 0) {
+    t.Errorf("The url wasn't formatted correctly. was expecting %s got %s", pair.result, r.Url)
+  }
+
 }
